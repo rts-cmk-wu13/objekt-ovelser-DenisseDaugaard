571737310ce17c3e6c6b9b9ae45df1e5f9ab6473
DenisseDaugaard <U2478723@rts-365.dk>
2025-02-03 11:35:22 +0100

    opgave 1

---
+++ script.js
@@ -0,0 +1,22 @@
+let person = {
+    firstname:"Denisse", 
+    lastname:"Daugaard",
+    age:29,
+    nationality:"Mexican",
+    pet:"hund",
+    petname:"Loui"
+}
+
+console.log(person.firstname);
+console.log(person.lastname);
+console.log(person.age);
+console.log(person.nationality);
+console.log(person.pet);
+console.log(person.petname);
+
+console.log(person["firstname"]);
+console.log(person["lastname"]);
+console.log(person["age"]);
+console.log(person["nationality"]);
+console.log(person["pet"]);
+console.log(person["petname"]);
