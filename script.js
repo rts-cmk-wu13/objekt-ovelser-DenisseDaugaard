1543917b0621e4b93315a7bd436ad3b2ef026163
DenisseDaugaard <U2478723@rts-365.dk>
2025-02-03 12:34:45 +0100

    opgave 2 løst

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// -----------Opgave 1--------------
+
 let person = {
     firstname:"Denisse", 
     lastname:"Daugaard",
@@ -7,16 +9,42 @@ let person = {
     petname:"Loui"
 }
 
-console.log(person.firstname);
-console.log(person.lastname);
-console.log(person.age);
-console.log(person.nationality);
-console.log(person.pet);
-console.log(person.petname);
+console.log("opgave 1a",person.firstname);
+console.log("opgave 1a",person.lastname);
+console.log("opgave 1a",person.age);
+console.log("opgave 1a",person.nationality);
+console.log("opgave 1a",person.pet);
+console.log("opgave 1a",person.petname);
 
-console.log(person["firstname"]);
-console.log(person["lastname"]);
-console.log(person["age"]);
-console.log(person["nationality"]);
-console.log(person["pet"]);
-console.log(person["petname"]);
+console.log("opgave 1b",person["firstname"]);
+console.log("opgave 1b",person["lastname"]);
+console.log("opgave 1b",person["age"]);
+console.log("opgave 1b",person["nationality"]);
+console.log("opgave 1b",person["pet"]);
+console.log("opgave 1b",person["petname"]);
+
+
+//---------------------- Opgave 2-----------------
+
+
+let person2 = {
+    firstname:"Denisse", 
+    lastname:"Daugaard",
+    age:29,
+    nationality:"Mexican",
+    hobbies:["singing", "dancing", "embroiding"],
+    pet:{
+        type: "hund",
+        petname:"Loui"
+    }
+}
+
+ person2.hobbies.forEach(function(hobby){
+    console.log("opgave 2a", hobby);
+});
+
+console.log("opgave 2b", person2.pet.type);
+console.log("opgave 2b", person2.pet.petname);
+
+//-------------Opgave 3-----------------------
+
